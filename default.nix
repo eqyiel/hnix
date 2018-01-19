8e49e56a545517ef3583fa944ac1edd09ac014dd
Ruben Maher <ruben@maher.fyi>
2018-01-19 14:12:48 +1030

    Include cabal in nix-shell environment

--- default.nix
+++ default.nix
@@ -1,11 +1,4 @@
-{ nixpkgs ? import <nixpkgs> {}, compiler ? null }:
-let
-  haskellPackages =
-    if compiler == null || !(nixpkgs.lib.hasAttr compiler nixpkgs.pkgs.haskell.packages)
-      # use the current default version
-      then nixpkgs.pkgs.haskellPackages
-      else nixpkgs.lib.attrByPath [compiler] {} nixpkgs.pkgs.haskell.packages;
-in
+{ nixpkgs ? import <nixpkgs> {}, haskellPackages ? nixpkgs.pkgs.haskellPackages }:
 
 haskellPackages.callPackage ./project.nix {
   pkgs = nixpkgs;
--- shell.nix
+++ shell.nix
@@ -1,2 +1,11 @@
 { nixpkgs ? import <nixpkgs> {}, compiler ? "ghc801" }:
-(import ./default.nix { inherit nixpkgs compiler; }).env
+
+let
+  haskellPackages =
+    if compiler == null || !(nixpkgs.lib.hasAttr compiler nixpkgs.pkgs.haskell.packages)
+      # use the current default version
+      then nixpkgs.pkgs.haskellPackages
+      else nixpkgs.lib.attrByPath [compiler] {} nixpkgs.pkgs.haskell.packages;
+in (nixpkgs.pkgs.haskell.lib.overrideCabal
+     (import ./default.nix { inherit nixpkgs haskellPackages; })
+       (args: { testToolDepends = [ haskellPackages.cabal-install ]; })).env
